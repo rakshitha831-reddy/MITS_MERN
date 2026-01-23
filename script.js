d0b54ba419de107f12e25f01e9611c4703a65446
Smile <rakshithareddy831@gmail.com>
2026-01-23 09:53:59 +0530

    java script

---
+++ script.js
@@ -0,0 +1,76 @@
+//console.log("Hello World");
+// var a = 5;//block scope
+// var a = 10;
+// console.log(a);
+
+// let b = 20;//block scope
+// console.log(b)
+
+// const c = 30;//block scope
+//const c = 40;
+// console.log(c);
+
+// var a = 10;
+// var str ="smile"//''//''
+// var bool = true;//boolean
+// var un;//unddefined
+// var nu = null;//null
+// var big = 124658787865n;
+// var symbol = Symbol("li");
+// console.log(typeof a);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof un);
+// console.log(typeof nu);
+// console.log(typeof big);
+// console.log(typeof symbol);
+//  Arthemetic 
+
+var a = 10;
+var b = 30;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//relational
+var a = 10;
+var b = 30;
+console.log(a>b);//false
+console.log(a<b);//true
+console.log(a>=b);//false
+console.log(a<=b);//false
+console.log(a==b);//ture
+console.log(a!=b);//false
+//loops
+// for loop
+//for(initialization;Condition;increment/decrement)
+    //statement
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+// let i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+// i = 1;
+// do{
+//     console.log('do.....while');
+// }while(i==0);
+
+// for(let i=1;i<=10;i++){
+//     if(i%2==0)
+//          console.log(i)
+// }
+// i=9;
+// if(i%2===0) console.log("even");
+// else console.log("odd");
+let mark=90;
+if(mark>=90) console.log("o grade")
+else if(mark>=80) console.log("A grade")
+else if(mark>=70) console.log("B grade")
+else console.log("fail")
+
+
