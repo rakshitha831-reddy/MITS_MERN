215b4b5700276673ac7e75c06845f15e1f398622
Smile <rakshithareddy831@gmail.com>
2026-01-23 16:49:34 +0530

    frontend

--- script.js
+++ script.js
@@ -26,51 +26,157 @@
 // console.log(typeof symbol);
 //  Arthemetic 
 
-var a = 10;
-var b = 30;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+// var a = 10;
+// var b = 30;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
 
-//relational
-var a = 10;
-var b = 30;
-console.log(a>b);//false
-console.log(a<b);//true
-console.log(a>=b);//false
-console.log(a<=b);//false
-console.log(a==b);//ture
-console.log(a!=b);//false
-//loops
-// for loop
-//for(initialization;Condition;increment/decrement)
-    //statement
-// for(let i=0;i<=10;i++){
-//     console.log(i);
+// //relational
+// var a = 10;
+// var b = 30;
+// console.log(a>b);//false
+// console.log(a<b);//true
+// console.log(a>=b);//false
+// console.log(a<=b);//false
+// console.log(a==b);//ture
+// console.log(a!=b);//false
+// //loops
+// // for loop
+// //for(initialization;Condition;increment/decrement)
+//     //statement
+// // for(let i=0;i<=10;i++){
+// //     console.log(i);
+// // }
+// // let i=1;
+// // while(i<=10){
+// //     console.log(i);
+// //     i++;
+// // }
+// // i = 1;
+// // do{
+// //     console.log('do.....while');
+// // }while(i==0);
+
+// // for(let i=1;i<=10;i++){
+// //     if(i%2==0)
+// //          console.log(i)
+// // }
+// // i=9;
+// // if(i%2===0) console.log("even");
+// // else console.log("odd");
+// let mark=90;
+// if(mark>=90) console.log("o grade")
+// else if(mark>=80) console.log("A grade")
+// else if(mark>=70) console.log("B grade")
+// else console.log("fail")
+
+// let arr = [10,20,30,40];
+// //for(variable variable_name in array_name){
+//     //statement
+// //}
+// for(let i in arr){
+//     console.log(i,arr[i]);
 // }
-// let i=1;
-// while(i<=10){
-//     console.log(i);
-//     i++;
+// var obj = {
+//     name:"ram",
+//     age:20
 // }
-// i = 1;
-// do{
-//     console.log('do.....while');
-// }while(i==0);
-
-// for(let i=1;i<=10;i++){
-//     if(i%2==0)
-//          console.log(i)
+// for(let i in obj){
+//     console.log(i,obj[i]);
 // }
-// i=9;
-// if(i%2===0) console.log("even");
-// else console.log("odd");
-let mark=90;
-if(mark>=90) console.log("o grade")
-else if(mark>=80) console.log("A grade")
-else if(mark>=70) console.log("B grade")
-else console.log("fail")
+// //for(variable var_name of arr_name ){
+// //statement
+// //}
+// for(let val of arr){
+//     console.log(val);
+// }
+// let arr = [2,3,7,8,6];
+// //map
+// var double_arr=arr.map((i)=>(i*2));
+// console.log(double_arr);
+// //filterr
+// var even = arr.filter((i)=>(i%2==0))
+// console.log(even);
+// //reduse
+// var total = arr.reduce((sum,i)=>(sum+i),0)
+// console.log(total);
+
+// var total_sum = arr.map((num)=>num*2).filter((num)=>(num%2===0)).reduce((sum,num)=>(sum+num),0)
+// console.log(total_sum);
+
+// var n = 7;
+// var flag=true;
+// if(n<=1) flag=false;
+// else{
+//     for(let i=2;i<=n/2;i++){
+//         if(n%i===0){
+//             flag=false;
+//             break;
+//         }
+//     }
+// }
+// console.log(flag?"Prime":"not prime");
+
+// var name="Lexiiii";
+// console.log('My name is ${name}');
+
+// var add = (a,b,callback)=>{
+//     var result=a+b;
+//     callback(result);
+// }
+// add(10,20,(res)=>{console.log(res)});
+
+// var demo =()=>{
+//     console.log("I am callback");
+// }
+// var main=(MyFunction)=>{
+//     MyFunction()
+// }
+// main(demo)
+
+// var promise=new Promise((resolve,reject)=>{
+//     var success=false;
+//     if(success){
+//         resolve("Promise Resolved");
+//     }
+//     else{
+//         reject("Promise Rejected")
+//     }
+// })
+// promise.then((res)=>console.log(res))
+// .catch((err)=>console.log(err))
+var promise=new Promise((resolve,reject)=>{
+    var success=true;
+    if(success){
+        setTimeout(()=>{
+        resolve("Promise Resolved");
+    },5000)
+    }
+    else{
+        reject("Promise Rejected")
+    }
+})
+promise.then((res)=>console.log(res))
+.catch((err)=>console.log(err))
+
+// const getData =()=>{
+//     return fetch('https://jsonplaceholder.typicode.com/posts')
+// }
+// getData().then((res)=>res.json())
+// .then((data)=>console.log(data))
+// .catch((err)=>console.log(err))
 
 
+const getData = async ()=>{
+    try{
+    var res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    var data = await res.json();
+    console.log(data);
+    }catch(err){
+    console.log(err);
+    }
+}
+getData()
